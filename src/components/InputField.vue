f1af222e598ffdd7cfc9ebf49ce39f3a2188021e
Slymee <jetli_rijan@hotmail.com>
2025-04-23 23:23:30 +0545

    Initial Commit

---
+++ src/components/InputField.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="mb-4">
+        <label :for="name" class="block text-sm font-medium text-gray-700 mb-1">{{ label }}</label>
+        <input :type="type" :name="name" :value="modelValue" @input="updateValue($event.target.value)"
+            :placeholder="placeholder"
+            class="block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        placeholder: {
+            type: String,
+            default: '',
+        },
+        modelValue: {
+            type: String,
+            default: '',
+        },
+    },
+    methods: {
+        updateValue(value) {
+            this.$emit('update:modelValue', value);
+        },
+    },
+};
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-slate-900 to-slate-700">
+        <div class="flex flex-col md:flex-row bg-[#fafafa] shadow-lg rounded-lg w-4/5 md:w-full max-w-7xl">
+            <div class="hidden md:block md:w-1/2">
+                Image
+            </div>
+
+            <div class="w-full md:w-1/2 p-8">
+                <h2 class="font-bold text-2xl mb-6 text-left">Login</h2>
+                <form action="">
+                    <InputField label="Username" name="username" v-model="username" placeholder="Enter Username" />
+                    <InputField label="Password" type="password" name="password" v-model="password"
+                        placeholder="Enter Password" />
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import InputField from '@/components/InputField.vue';
+
+export default {
+    components: {
+        InputField,
+    },
+    data() {
+        return {
+            username: '',
+            password: '',
+        };
+    },
+    methods: {
+        handleSubmit() {
+            console.log('Email:', this.username);
+            console.log('Password:', this.password);
+        },
+    },
+};
+</script>
