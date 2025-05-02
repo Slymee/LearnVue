4a7453adcc0af24db8614248e2db0d24c9e9f526
Slymee <jetli_rijan@hotmail.com>
2025-05-02 16:54:20 +0545

    NavBar and router-link

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="flex justify-between items-center p-4 bg-gray-100">
+        <div class="text-lg font-bold">Logo</div>
+        <div>
+            <ul class="flex space-x-4">
+                <li><router-link to="/" class="hover:text-blue-500 cursor-pointer">Home</router-link></li>
+                <li><router-link to="/about" class="hover:text-blue-500 cursor-pointer">About</router-link></li>
+                <li><router-link to="/login" class="hover:text-blue-500 cursor-pointer">Login</router-link></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+</script>
